26259e0ffbbb652a7f2c4270b228a746c5400141
Balakosh <akos.balassa@live.com>
2024-05-20 11:49:20 +0200

    feat: Stockfish now added to the cmake project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+//#include "src/position.h"
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    //Stockfish::Position pos;
+    //pos.init();
+    //auto fen = pos.fen();
+
+    //std::cout << fen << std::endl;
 
     return 0;
 }

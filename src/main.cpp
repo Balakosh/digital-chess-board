5a9a0462d6c16bac14c8739f2efeb5c30345321c
Balakosh <akos.balassa@live.com>
2024-05-25 17:09:16 +0200

    feat: added e4 move to test the bitboard and position class

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,27 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
+    Stockfish::Bitboards::init();
+    Stockfish::Position::init();
     Stockfish::Position pos;
-    pos.init();
-    auto fen = pos.fen();
+    Stockfish::Bitboard bitboard;
+    Stockfish::StateInfo si;
 
-    std::cout << fen << std::endl;
+    // Set up the position using a FEN string
+    std::string fenStr = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"; // Starting position
+    pos.set(fenStr, false, &si);
+    Stockfish::Move move(Stockfish::SQ_E2, Stockfish::SQ_E4);
+    const bool is_legal_move = pos.legal(move);
+    std::cout << "is_legal_move: " << is_legal_move << std::endl;
+    Stockfish::StateInfo newstate;
+    pos.do_move(move, newstate);
+    bitboard = pos.pieces();
+    Stockfish::Piece piece = pos.piece_on(Stockfish::Square::SQ_A1);
+    std::cout << "piece: " << piece << std::endl;
+
+    // Call the fen() method and print the FEN string
+    std::cout << "FEN: " << pos.fen() << std::endl;
+    std::cout << "bitboard: " << Stockfish::Bitboards::pretty(bitboard) << std::endl;
 
     return 0;
 }

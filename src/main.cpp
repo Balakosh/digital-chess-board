2f98b1c76468ce280087d6eb9a62141300874780
Balakosh <akos.balassa@live.com>
2024-05-25 17:15:13 +0200

    refactor: fixed some warnings

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ int main() {
 
     Stockfish::Bitboards::init();
     Stockfish::Position::init();
-    Stockfish::Position pos;
+    Stockfish::Position pos = {};
     Stockfish::Bitboard bitboard;
-    Stockfish::StateInfo si;
+    Stockfish::StateInfo si = {};
 
     // Set up the position using a FEN string
     std::string fenStr = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"; // Starting position
@@ -17,8 +17,8 @@ int main() {
     Stockfish::Move move(Stockfish::SQ_E2, Stockfish::SQ_E4);
     const bool is_legal_move = pos.legal(move);
     std::cout << "is_legal_move: " << is_legal_move << std::endl;
-    Stockfish::StateInfo newstate;
-    pos.do_move(move, newstate);
+    Stockfish::StateInfo new_state = {};
+    pos.do_move(move, new_state);
     bitboard = pos.pieces();
     Stockfish::Piece piece = pos.piece_on(Stockfish::Square::SQ_A1);
     std::cout << "piece: " << piece << std::endl;

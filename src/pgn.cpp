942456e483f9e7e5355b8c777404af4b7351c378
Balakosh <akos.balassa@live.com>
2024-05-25 19:39:03 +0200

    feat: started to implement pgn recorder.

---
+++ src/pgn.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Akeman on 25-May-24.
+//
+
+#include <sstream>
+
+#include "pgn.h"
+
+void pgn::record_move(Stockfish::Move move, Stockfish::Position& pos) {
+    const std::string move_stirng = move_to_string(move, pos);
+    moves.push_back(move_stirng);
+}
+
+std::string pgn::move_to_string(Stockfish::Move move, Stockfish::Position& pos)
+{
+    Stockfish::Piece piece = pos.piece_on(move.from_sq());
+
+    return "";
+}
+
+std::string pgn::get_pgn() {
+    std::ostringstream pgn_string;
+    for (size_t i = 0; i < moves.size(); ++i) {
+        if (i % 2 == 0) {
+            pgn_string << (i / 2 + 1) << ". ";
+        }
+        pgn_string << moves[i] << " ";
+    }
+    return pgn_string.str();
+}
+

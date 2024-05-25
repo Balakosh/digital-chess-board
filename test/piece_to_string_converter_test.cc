7631587efe604f00e2f29b55da09c4764e0f8a35
Balakosh <akos.balassa@live.com>
2024-05-25 21:22:53 +0200

    test: PieceToStringConverterTest.Convert implemented as a parameterized test

--- test/piece_to_string_converter_test.cc
+++ test/piece_to_string_converter_test.cc
@@ -3,10 +3,27 @@
 #include "../include/piece_to_string_converter.h"
 #include "../thirdparty/Stockfish/src/types.h"
 
-TEST(piece_to_string_converter_test, Convert) {
-    Stockfish::Piece piece = Stockfish::B_BISHOP;
+struct PieceToStringTestParams {
+    Stockfish::Piece piece;
+    std::string expected_string;
+};
 
-    const std::string piece_str = piece_to_string_converter::convert(piece);
+class PieceToStringConverterTest : public ::testing::TestWithParam<PieceToStringTestParams> {};
 
-    EXPECT_EQ(piece_str, "b");
+TEST_P(PieceToStringConverterTest, Convert) {
+    PieceToStringTestParams params = GetParam();
+
+    const std::string piece_str = piece_to_string_converter::convert(params.piece);
+
+    EXPECT_EQ(piece_str, params.expected_string);
 }
+
+INSTANTIATE_TEST_SUITE_P(
+        PieceToStringTests,
+        PieceToStringConverterTest,
+        ::testing::Values(
+                PieceToStringTestParams{Stockfish::B_BISHOP, "B"},
+                PieceToStringTestParams{Stockfish::W_KING, "K"},
+                PieceToStringTestParams{Stockfish::B_KNIGHT, "N"}
+        )
+);

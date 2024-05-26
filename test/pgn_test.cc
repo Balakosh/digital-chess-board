83ee1c8fe478411e15013b707aaf303a79f65915
Balakosh <akos.balassa@live.com>
2024-05-26 11:39:02 +0200

    test: started to implement PGNTest.move_to_string_test

--- test/pgn_test.cc
+++ test/pgn_test.cc
@@ -1,2 +1,19 @@
 #include <gtest/gtest.h>
 
+#include "../include/pgn.h"
+#include "../thirdparty/Stockfish/src/types.h"
+
+TEST(PGNTest, move_to_string_test) {
+    Stockfish::Position pos = {};
+    Stockfish::StateInfo si = {};
+
+    Stockfish::Bitboards::init();
+    Stockfish::Position::init();
+
+    std::string start_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    pos.set(start_position, false, &si);
+
+    const std::string move = pgn::move_to_string(Stockfish::Square::SQ_E2, Stockfish::Square::SQ_E4, pos);
+
+    ASSERT_EQ("e4", move);
+}

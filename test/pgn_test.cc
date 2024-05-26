c4b170bd7c46716ff915197801242ea4a8cd10aa
Balakosh <akos.balassa@live.com>
2024-05-26 14:01:02 +0200

    test: PGNTest is now a test fixture

--- test/pgn_test.cc
+++ test/pgn_test.cc
@@ -3,17 +3,37 @@
 #include "../include/pgn.h"
 #include "../thirdparty/Stockfish/src/types.h"
 
-TEST(PGNTest, move_to_string_test) {
-    Stockfish::Position pos = {};
-    Stockfish::StateInfo si = {};
+class PGNTest : public ::testing::Test {
+protected:
+    Stockfish::Position pos;
+    Stockfish::StateInfo si;
 
-    Stockfish::Bitboards::init();
-    Stockfish::Position::init();
+    void SetUp() override {
+        Stockfish::Bitboards::init();
+        Stockfish::Position::init();
 
-    std::string start_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    pos.set(start_position, false, &si);
+        std::string start_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        pos.set(start_position, false, &si);
+    }
 
+    void TearDown() override {
+    }
+};
+
+TEST_F(PGNTest, Pawn_to_e4) {
     const std::string move = pgn::move_to_string(Stockfish::Square::SQ_E2, Stockfish::Square::SQ_E4, pos);
 
     ASSERT_EQ("e4", move);
+}
+
+TEST_F(PGNTest, Pawn_to_d4) {
+    const std::string move = pgn::move_to_string(Stockfish::Square::SQ_D2, Stockfish::Square::SQ_D4, pos);
+
+    ASSERT_EQ("d4", move);
+}
+
+TEST_F(PGNTest, Knight_f3) {
+    const std::string move = pgn::move_to_string(Stockfish::Square::SQ_G1, Stockfish::Square::SQ_F3, pos);
+
+    ASSERT_EQ("Nf3", move);
 }

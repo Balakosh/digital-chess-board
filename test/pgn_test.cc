4b22804644176f0fb9aa168503639e33e9cf31f4
Balakosh <akos.balassa@live.com>
2024-05-26 14:21:09 +0200

    test: starting positon for the tests will be a parameter for the parameterized test to be able to check other moves than from the starting postion

--- test/pgn_test.cc
+++ test/pgn_test.cc
@@ -5,6 +5,7 @@
 #include "../thirdparty/Stockfish/src/types.h"
 
 struct PGNTestParams {
+    std::string fen_position;
     Stockfish::Square from;
     Stockfish::Square to;
     std::string expected_move;
@@ -18,9 +19,6 @@ protected:
     void SetUp() override {
         Stockfish::Bitboards::init();
         Stockfish::Position::init();
-
-        std::string start_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-        pos.set(start_position, false, &si);
     }
 
     void TearDown() override {
@@ -29,6 +27,8 @@ protected:
 
 TEST_P(PGNTest, MoveToStringTest) {
     const auto& params = GetParam();
+    pos.set(params.fen_position, false, &si);
+
     const std::string move = pgn::move_to_string(params.from, params.to, pos);
 
     ASSERT_EQ(params.expected_move, move);
@@ -38,9 +38,10 @@ INSTANTIATE_TEST_SUITE_P(
         PGNTests,
         PGNTest,
         ::testing::Values(
-                PGNTestParams{Stockfish::Square::SQ_E2, Stockfish::Square::SQ_E4, "e4"},
-                PGNTestParams{Stockfish::Square::SQ_D2, Stockfish::Square::SQ_D4, "d4"},
-                PGNTestParams{Stockfish::Square::SQ_G1, Stockfish::Square::SQ_F3, "Nf3"},
-                PGNTestParams{Stockfish::Square::SQ_B1, Stockfish::Square::SQ_C3, "Nc3"}
+                PGNTestParams{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Stockfish::Square::SQ_E2, Stockfish::Square::SQ_E4, "e4"},
+                PGNTestParams{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Stockfish::Square::SQ_D2, Stockfish::Square::SQ_D4, "d4"},
+                PGNTestParams{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Stockfish::Square::SQ_G1, Stockfish::Square::SQ_F3, "Nf3"},
+                PGNTestParams{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Stockfish::Square::SQ_B1, Stockfish::Square::SQ_C3, "Nc3"},
+                PGNTestParams{"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1", Stockfish::Square::SQ_G8, Stockfish::Square::SQ_F6, "Nf6"}
         )
 );

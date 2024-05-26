e030f85b4427e92e3f6ac136d0730a41e7168f14
Balakosh <akos.balassa@live.com>
2024-05-26 14:15:10 +0200

    feat: pgn::move_to_string() will check if the move is legal.

--- src/pgn.cpp
+++ src/pgn.cpp
@@ -16,11 +16,17 @@ void pgn::record_move(Stockfish::Square from, Stockfish::Square to, Stockfish::P
 std::string pgn::move_to_string(Stockfish::Square from, Stockfish::Square to, Stockfish::Position& pos)
 {
     Stockfish::Move move(from, to);
-    Stockfish::Piece piece = pos.piece_on(move.from_sq());
-    const std::string piece_str = piece_to_string_converter::convert(piece);
-    const std::string square_str = square_to_string_converter::convert(to);
+    const bool is_legal_move = pos.legal(move);
 
-    return piece_str + square_str;
+    if (is_legal_move) {
+        Stockfish::Piece piece = pos.piece_on(move.from_sq());
+        const std::string piece_str = piece_to_string_converter::convert(piece);
+        const std::string square_str = square_to_string_converter::convert(to);
+
+        return piece_str + square_str;
+    } else {
+        return "illegal move!";
+    }
 }
 
 std::string pgn::get_pgn() {
